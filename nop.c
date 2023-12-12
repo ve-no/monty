d2fadb4a110d0539da493b5cef4f28f39b8f70b3
ve-no <allal.elbakourii@gmail.com>
2023-12-12 16:09:06 +0100

    Add f_nop function to nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * f_nop - Does nothing.
+ * @head: Pointer to the stack head.
+ * @counter: Line number.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+  (void)head;
+  (void)counter;
+}
