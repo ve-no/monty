eeac28dc12982776459dc7d6d3ae8440850e0142
ve-no <allal.elbakourii@gmail.com>
2023-12-12 16:08:42 +0100

    Add free_stack function to free a doubly linked list

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_stack - frees a doubly linked list
+ * @head: head of the stack
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *current = head;
+	stack_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
