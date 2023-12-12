8d11208ffb305b07561243d10ab313aaaebefd33
ve-no <allal.elbakourii@gmail.com>
2023-12-12 16:09:45 +0100

    Add f_rotl function to rotate the stack to the top

---
+++ rotl.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * f_rotl - Rotates the stack to the top
+ * @head: Pointer to the stack head
+ * @counter: Line number
+ *
+ * Return: No return value
+ */
+void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+
+	aux = (*head)->next;
+	aux->prev = NULL;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
