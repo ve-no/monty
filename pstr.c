f0c82c2edccbfcfa92bd597511e4029ebc4946ff
ve-no <allal.elbakourii@gmail.com>
2023-12-12 16:09:31 +0100

    Add pstr function to print string from stack

---
+++ pstr.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * f_pstr - prints the string starting at the top of the stack,
+ * followed by a new line
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		putchar(h->n);
+		h = h->next;
+	}
+	putchar('\n');
+}
