8cf00e9444086746cb3a1a633a646510bb7fb313
ve-no <allal.elbakourii@gmail.com>
2023-12-12 16:09:00 +0100

    Add f_mul function to multiply top two elements of the stack

---
+++ mul.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * f_mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h = *head;
+
+	if (h == NULL || h->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	h->next->n *= h->n;
+	*head = h->next;
+	free(h);
+}
