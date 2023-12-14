cca6084da69e2a052c4d8d0eeb4f4ca7d4ed8784
Allal EL BAKOURI <95944107+ve-no@users.noreply.github.com>
2023-12-14 11:13:04 +0100

    norm

--- main.c
+++ main.c
@@ -15,13 +15,11 @@ int main(int argc, char *argv[])
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
 	bus.file = file;
 
@@ -30,7 +28,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	while (read_line > 0)
 	{
 		content = NULL;
@@ -45,9 +42,7 @@ int main(int argc, char *argv[])
 
 		free(content);
 	}
-
 	free_stack(stack);
 	fclose(file);
-
-	return 0;
+	return (0);
 }
